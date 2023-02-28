2826fab1b371407d97df5e6099bc5badf3231b59
Michael Plumlee <michaelplumlee95@gmail.com>
2023-02-28 11:26:24 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+]
+
+
+function destructivelyAppendCat(catName) {
+    cats.push(catName)
+}
+
+function destructivelyPrependCat(catName) {
+    cats.unshift(catName)
+}
+
+function destructivelyRemoveLastCat(catName) {
+    cats.pop(catName)
+}
+
+function destructivelyRemoveFirstCat(catName) {
+    cats.shift(catName)
+}
+
+function appendCat(catName) {
+    const newCats = [...cats, catName]
+    return newCats;
+}
+
+function prependCat(catName) {
+    const newCats = [catName, ...cats]
+    return newCats;
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+    
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
+
+
